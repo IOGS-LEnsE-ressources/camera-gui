4766ed249a26304310a365abe8325de1063dfe2d
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-03-07 15:04:11 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../../progs/Basler'))
 
 project = 'Camera GUI'
 copyright = '2024, Julien Villemejane'
-author = 'Julien Villemejane'
+author = 'Julien Villemejane <>'
 release = '0.1'
 
 
