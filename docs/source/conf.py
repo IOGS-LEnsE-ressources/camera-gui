c63bc31639eed4f35d3649daceca393f5243c804
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-03-08 16:36:04 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ extensions = [
     'linuxdoc.rstFlatTable'
 ]
 
-autodoc_mock_imports = ["pypylon"]
+autodoc_mock_imports = ["pypylon","numpy"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
