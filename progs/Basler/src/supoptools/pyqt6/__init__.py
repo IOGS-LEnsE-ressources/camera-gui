0eb43c20d0f0610424d6a91a3951670b32d1ac8f
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-06-04 21:18:49 +0200

    Move IDS/Basler to lensecam

--- progs/Basler/src/supoptools/pyqt6/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = [
-    "widget_slider",      # refers to the 'widget_slider.py' file
-    "widget_histogram",   # refers to the 'widget_histogram.py' file
-    "widget_image_display",     # refers to the 'widget_image_display.py' file
-]
