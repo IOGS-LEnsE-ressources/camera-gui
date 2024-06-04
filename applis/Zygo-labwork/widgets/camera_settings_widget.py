035999998a4ab907f20f528ddcc4c3a188e6d36a
Dorian Mendes <dorian.mendes63530@gmail.com>
2024-06-04 21:34:57 +0200

    Update widgets including translation of lensepy

--- applis/Zygo-labwork/widgets/camera_settings_widget.py
+++ applis/Zygo-labwork/widgets/camera_settings_widget.py
@@ -27,6 +27,7 @@ from PyQt6.QtCore import pyqtSignal, QTimer, Qt
 from PyQt6.QtGui import QPixmap
 from slider_block import SliderBlock
 import numpy as np
+from lensepy import load_dictionary, translate
 
 # %% To add in lensepy librairy
 # Colors
@@ -50,67 +51,6 @@ actived_button = f"background-color:{ORANGE_IOGS}; color:white; font-size:15px; 
 # Translation
 # -----------
 dictionary = {}
-
-def load_dictionary(language_path: str) -> None:
-    """
-    Load a dictionary from a CSV file based on the specified language.
-
-    Parameters
-    ----------
-    language : str
-        The language path to specify which CSV file to load.
-
-    Returns
-    -------
-    None
-
-    Notes
-    -----
-    This function reads a CSV file that contains key-value pairs separated by semicolons (';')
-    and stores them in a global dictionary variable. The CSV file may contain comments
-    prefixed by '//', which will be ignored.
-
-    The file should have the following format:
-        // comment
-        // comment
-        key_1 ; language_word_1
-        key_2 ; language_word_2
-
-    The function will strip any leading or trailing whitespace from the keys and values.
-
-    See Also
-    --------
-    numpy.genfromtxt : Load data from a text file, with missing values handled as specified.
-    """
-    global dictionary
-    dictionary = {}
-
-    # Read the CSV file, ignoring lines starting with '//'
-    data = np.genfromtxt(language_path, delimiter=';', dtype=str, comments='#', encoding='UTF-8')
-
-    # Populate the dictionary with key-value pairs from the CSV file
-    for key, value in data:
-        dictionary[key.strip()] = value.strip()
-
-def translate(key: str) -> str:
-    """
-    Translate a given key to its corresponding value.
-
-    Parameters
-    ----------
-    key : str
-        The key to translate.
-
-    Returns
-    -------
-    str
-        The translated value corresponding to the key. If the key does not exist, it returns the key itself.
-
-    """    
-    if ('dictionary' in globals()) and (key in dictionary):
-        return dictionary[key]
-    else:
-        return key
 
 # %% Params
 BUTTON_HEIGHT = 60 #px
