8acfc92c3e21161a103c60b3d93e5ef219aba2a3
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-06-04 16:55:38 +0200

    Update

--- applis/Zygo-labwork/drivers/nidaq_piezo.py
+++ applis/Zygo-labwork/drivers/nidaq_piezo.py
@@ -16,7 +16,7 @@ https://iogs-lense-ressources.github.io/camera-gui/contents/applis/appli_Zygo_la
 """
 
 import nidaqmx
-class CameraIds:
+class NIDaqPiezo:
     """Class to communicate with a DAQ module from NI.
 
     """
@@ -25,3 +25,66 @@ class CameraIds:
         """Initialize the object."""
         # Camera device
         pass
+
+    def read_adc(self, chan: str = "Dev1/ai0"):
+        result = None
+        with nidaqmx.Task() as task:
+            task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+            result = task.read()
+        return result
+
+if __name__ == '__main__':
+    local_system = nidaqmx.system.System.local()
+    driver_version = local_system.driver_version
+    print(driver_version)
+
+    # print(len(local_system.devices))
+
+    import sys
+    import numpy as np
+    from PyQt6.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
+
+    # Créer une application PyQt6
+    app = QApplication(sys.argv)
+
+    # Créer un tableau numpy
+    data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+    # Créer un widget de tableau
+    table_widget = QTableWidget()
+
+    # Définir le nombre de lignes et de colonnes du tableau
+    table_widget.setRowCount(data.shape[0])
+    table_widget.setColumnCount(data.shape[1])
+
+    # Remplir les cellules du tableau avec les données du tableau numpy
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            table_widget.setItem(i, j, QTableWidgetItem(str(data[i, j])))
+
+    # Créer un widget principal
+    main_widget = QWidget()
+
+    # Créer une mise en page verticale pour le widget principal
+    layout = QVBoxLayout()
+
+    # Ajouter le widget de tableau à la mise en page
+    layout.addWidget(table_widget)
+
+    # Définir la mise en page pour le widget principal
+    main_widget.setLayout(layout)
+
+    # Afficher le widget principal
+    main_widget.show()
+
+    # Exécuter l'application PyQt6
+    sys.exit(app.exec())
+
+    '''
+    for device in local_system.devices:
+        print(
+            "Device Name: {0}, Product Category: {1}, Product Type: {2}".format(
+                device.name, device.product_category, device.product_type
+            )
+        )
+    '''
