c91548839d5c56cbbb862df61fb3a555f3ff4c30
Julien Villemejane <julien.villemejane@institutoptique.fr>
2024-11-07 21:58:23 +0100

    Update Zygo Labwork GUI - Adding test on zernike and aberration correction

--- applis/Zygo-labwork_v2/_test/unwrap_process.py
+++ applis/Zygo-labwork_v2/_test/unwrap_process.py
@@ -15,14 +15,40 @@ import numpy as np
 from scipy.ndimage import gaussian_filter, uniform_filter
 from scipy.interpolate import griddata
 from skimage.restoration import unwrap_phase
-import h5py
+from hariharan_algorithm import *
+from zernike_coefficents import *
+from polar_cartesian_transformations import *
 
 PI = np.pi
 
-def read_mat_file(file_path):
+def read_mat_file(file_path: str) -> dict:
+    """
+    Load data and masks from a .mat file.
+    The file must contain a set of 5 images (Hariharan algorithm) in a dictionary key called "Images".
+    Additional masks can be included in a dictionary key called "Masks".
+
+    :param file_path: Path and name of the file to load.
+    :return: Dictionary containing at least np.ndarray including in the "Images"-key object.
+    """
     data = scipy.io.loadmat(file_path)
-    print(data.keys())
     return data
+
+def write_mat_file(file_path, images: np.ndarray, masks: np.ndarray = None):
+    """
+    Load data and masks from a .mat file.
+    The file must contain a set of 5 images (Hariharan algorithm) in a dictionary key called "Images".
+    Additional masks can be included in a dictionary key called "Masks".
+
+    :param file_path: Path and name of the file to write.
+    :param images: Set of images to save.
+    :param masks: Set of masks to save. Default None.
+    """
+    data = {
+        'Images': images
+    }
+    if masks is not None:
+        data['Masks'] = masks
+    scipy.io.savemat(file_path, data)
 
 def split_3d_array(array_3d):
     # Ensure the array has the expected shape
@@ -33,68 +59,20 @@ def split_3d_array(array_3d):
     arrays = [array_3d[:, :, i].astype(np.float32) for i in range(5)]
     return arrays
 
-def hariharan_algorithm(intensity: list[np.ndarray]) -> np.ndarray:
-    """
-    Apply the Hariharan phase demodulation algorithm to a set of intensity measurements.
-
-    Parameters
-    ----------
-    intensity : list[np.ndarray]
-        First intensity measurement, I_1 = I_0 * (1 + C * cos(phi)).
-        Second intensity measurement, I_2 = I_0 * (1 + C * cos(phi + π/2)).
-        Third intensity measurement, I_3 = I_0 * (1 + C * cos(phi + π)).
-        Fourth intensity measurement, I_4 = I_0 * (1 + C * cos(phi + 3π/2)).
-        Fifth intensity measurement, I_5 = I_0 * (1 + C * cos(phi + 2π)).
-
-    Returns
-    -------
-    np.ndarray
-        The demodulated phase phi array.
-
-    Notes
-    -----
-    This function calculates the demodulated phase using the Hariharan algorithm, which is a method for phase recovery in interferometry.
-
-    The five intensity measurements are taken with phase shifts of π/2 between them, specifically:
-    - I_1 = I_0(1 + C * cos(phi))
-    - I_2 = I_0(1 + C * cos(phi + pi/2))
-    - I_3 = I_0(1 + C * cos(phi + pi))
-    - I_4 = I_0(1 + C * cos(phi + 3*pi/2))
-    - I_5 = I_0(1 + C * cos(phi + 2*pi))
-
-    The returned phase values are in radians.
-
-    This algorithm was originally published in:
-    P. Hariharan, B. F. Oreb, and T. Eiju, "Digital phase-shifting interferometry: a simple error-compensating phase calculation algorithm," Appl. Opt. 26, 2504-2506 (1987).
-    Available at: https://opg.optica.org/ao/fulltext.cfm?uri=ao-26-13-2504&id=168363
-    """
-    num = 2 * (intensity[3] - intensity[1])
-    denum = 2 * intensity[2] - intensity[4] - intensity[0]
-    return np.arctan2(num, denum)
-
-def statistique_surface(surface):
+def surface_statistics(surface):
     # Calcul de PV (Peak-to-Valley)
     PV = np.nanmax(surface) - np.nanmin(surface)
     RMS = np.nanstd(surface)
     return PV, RMS
 
+
+
 ## Read data from MatLab file
 data = read_mat_file("../_data/imgs2.mat")
 images_mat = data['Imgs']
 images = split_3d_array(images_mat)
-
-print(images[0].shape)
-
-## Test with NPZ file / To save images after acquiring !
-
-# Sauvegarde dans un fichier .npz
-#np.savez('matrices.npz', *images_mat)
-
-# Chargement du fichier .npz
-#loaded_data = np.load('matrices.npz')
-#images = [loaded_data[f'arr_{i}'] for i in range(len(loaded_data.files))]
-
-print(type(images))
+## Write a new set of data into a MatLab file
+new_data = np.stack((images), axis=2).astype(np.uint8)
 
 # Display images
 '''
@@ -112,6 +90,12 @@ mask[200:400, 950:1600] = 1
 mask[300:400, 880:1800] = 1
 mask[1200:1400, 1350:1750] = 1
 
+write_mat_file('test.mat', new_data, mask.astype(np.uint8))
+data2 = read_mat_file('test.mat')
+images_mat = data2['Images']
+images = split_3d_array(images_mat)
+mask = data2['Masks']   # TO DO : add a test on the size of 'Masks'
+
 '''
 plt.imshow(images[0]*mask, cmap='magma')
 plt.show()
@@ -120,6 +104,7 @@ plt.show()
 ## TO DO : test with masks selection by user...
 
 ## Gaussian filter on image*
+print('Opening Images...')
 sigma = 10
 images_filtered = list(map(lambda x:gaussian_filter(x, sigma), images))
 
@@ -127,9 +112,10 @@ for i, img in enumerate(images_filtered):
     plt.subplot(1,5,i+1)
     plt.imshow(img[400:1200, 750:1900], cmap='gray')
     plt.axis('off')
-plt.show()
+#plt.show()
 
 ## Calculation of the phase by Hariharan algorithm
+print('Calculating Phase by Hariharan Algo...')
 wrapped_phase = hariharan_algorithm(images_filtered)
 # Array for displaying data on 3D projection
 x = np.arange(wrapped_phase.shape[1])
@@ -143,9 +129,10 @@ surface = ax.plot_surface(X[400:1200, 750:1900], Y[400:1200, 750:1900], wrapped_
 ax.set_title('Unwrapped surface')
 cbar = fig.colorbar(surface, ax=ax, shrink=0.5, aspect=10)
 cbar.set_label(r'Default magnitude ($\lambda$)')
-plt.show()
+#plt.show()
 
 ## Unwrap phase
+print('Unwrapping Phase...')
 unwrapped_phase = unwrap_phase(wrapped_phase)/(2*np.pi)
 
 # Display of the surface
@@ -155,9 +142,59 @@ surface = ax.plot_surface(X[400:1200, 750:1900], Y[400:1200, 750:1900], unwrappe
 ax.set_title('Unwrapped surface')
 cbar = fig.colorbar(surface, ax=ax, shrink=0.5, aspect=10)
 cbar.set_label(r'Default magnitude ($\lambda$)')
+#plt.show()
+
+## Statistics
+PV, RMS = surface_statistics(unwrapped_phase[400:1200, 750:1900])
+print(f"PV: {PV:.2f} λ | RMS: {RMS:.2f} λ")
+
+
+## Zernike coefficients
+print('Calculating Zernike coefficients...')
+max_order = 3
+coeffs = get_zernike_coefficient(unwrapped_phase, max_order=max_order)
+
+plt.figure()
+plt.plot(coeffs)
+plt.title('Zernike Coefficients')
+
+# Remove specified aberration
+aberrations_considered = np.ones(max_order, dtype=int)
+print(aberrations_considered)
+
+corrected_phase, XX, YY = remove_aberration(unwrapped_phase[400:1200, 750:1900], aberrations_considered)
+
+PV, RMS = surface_statistics(corrected_phase[400:1200, 750:1900])
+print(f"PV: {PV:.2f} λ | RMS: {RMS:.2f} λ")
+
+## Display initial and corrected phase
+# Création de la figure et des sous-graphiques en 3D
+fig = plt.figure(figsize=(14, 6))
+
+# Initial phase with aberrations
+min_value = np.round(np.min(unwrapped_phase[400:1200, 750:1900]), 0) - 1
+max_value = np.round(np.max(unwrapped_phase[400:1200, 750:1900]), 0) + 1
+
+ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+surf1 = ax1.plot_surface(XX, YY, unwrapped_phase[400:1200, 750:1900], cmap='viridis')
+ax1.set_zlim([min_value, max_value])
+ax1.set_title('Initial phase with aberrations')
+fig.colorbar(surf1, ax=ax1, shrink=0.5, aspect=10)
+
+# Corrected phase without specified aberrations
+ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+ax2.set_xlabel('X')
+ax2.set_ylabel('Y')
+surf2 = ax2.plot_surface(XX, YY, corrected_phase, cmap='viridis')
+ax2.set_zlim([min_value, max_value])
+ax2.set_title('Corrected phase without specified aberrations')
+fig.colorbar(surf2, ax=ax2, shrink=0.5, aspect=10)
+
+# Ajustement des paramètres de la figure
+fig.tight_layout()
+
 plt.show()
 
 
-# Statistics
-PV, RMS = statistique_surface(unwrapped_phase)
-print(f"PV: {PV:.2f} λ | RMS: {RMS:.2f} λ")
