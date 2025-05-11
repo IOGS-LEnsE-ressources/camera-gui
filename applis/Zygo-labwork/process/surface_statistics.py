f0e029d2b8d0656bfcf180e91824bf8ffe52258c
Julien Villemejane <julien.villemejane@institutoptique.fr>
2025-05-11 15:15:18 +0200

    Remove Zygo APP -> transfer to an independant GitHub repository

--- applis/Zygo-labwork/process/surface_statistics.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def statistique_surface(surface):
-    # Calcul de PV (Peak-to-Valley)
-    PV = np.nanmax(surface) - np.nanmin(surface)
-    RMS = np.nanstd(surface)
-    return PV, RMS
--- applis/Zygo-labwork_v2 test/drivers/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = [
-    "camera_basler",        # refers to the 'camera_basler.py' file
-    "camera_basler_widget", # refers to the 'camera_basler_widget.py' file
-    "camera_list",          # refers to the 'camera_list.py' file
-]
