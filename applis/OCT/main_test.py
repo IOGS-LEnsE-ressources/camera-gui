0c83af8f120bc5f8b25802059c9f57113605b088
noamchoppinet <noam.choppinet@outlook.fr>
2025-06-03 16:59:26 +0200

    widgets des entrées et capture des images

--- applis/OCT/main_test.py
+++ applis/OCT/main_test.py
@@ -2,26 +2,35 @@ from models.motor_control import *
 from _tests.camera_test import *
 import numpy as np
 
-exposure = 1000
+exposure = 1500
 N = 10
-n=  5
+n= 9
 delta_z = 0.1
 
 P = Piezo()
-M = Motor()
+size = 3
+position_initiale = 2
 
 cam = find_camera()
 cam.set_exposure(exposure)
 
-for i in range(n):
-    image1 = cam.get_images(N)
-    P.set_voltage_piezo(2)
-    image2 = cam.get_images(N)
+fig, axs = plt.subplots(size, size, figsize=(8, 8))
+
+for i, ax in enumerate(axs.flat):
+    if i<n:
+        image1 = cam.get_images(N)
+        P.set_voltage_piezo(0.2*i+0.02)
+        image2 = cam.get_images(N)
+        P.set_zero_piezo()
 
     image = (np.mean(image1, axis=0) - np.mean(image2, axis=0))**2
 
-    plt.figure()
-    plt.imshow(image, cmap='gray')
-    M.move_motor(0.1+delta_z*i)
+    max_image = image.max()
 
+    if max_image>0:
+        image = abs(image/max_image)
+
+    ax.imshow(image, cmap='gray')
+
+cam.disconnect()
 plt.show()
---
+++ applis/OCT/views/acquisition.py
@@ -0,0 +1,163 @@
+import cv2
+import numpy as np
+import sys
+from PyQt6.QtWidgets import (
+    QWidget, QGridLayout, QVBoxLayout,
+    QLabel, QComboBox, QPushButton, QFrame,
+    QSizePolicy, QSpacerItem, QMainWindow, QHBoxLayout, QApplication, QSlider, QLineEdit, QFileDialog)
+from PyQt6.QtCore import Qt, pyqtSignal
+
+
+class AcquisitionView(QWidget):
+
+    acqThread = pyqtSignal(str)
+
+    def __init__(self, parent = None):
+        super().__init__()
+
+        self.setWindowTitle("Acquisition")
+        self.parent = parent
+
+        layout = QVBoxLayout()
+
+        directory_layout = QHBoxLayout()
+        name_layout = QHBoxLayout()
+        step_size_layout = QHBoxLayout()
+        step_num_layout = QHBoxLayout()
+        buttons_layout = QHBoxLayout()
+
+        ### Destination
+
+        self.directory_label = QLabel("Destination : ")
+        self.directory_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+
+        self.directory = QLineEdit("")
+        self.directory.setEnabled = True
+        self.directory.editingFinished.connect(self.directory_action)
+
+        self.search = QPushButton("Parcourir")
+        self.search.clicked.connect(self.directory_action)
+
+        directory_layout.addWidget(self.directory_label)
+        directory_layout.addWidget(self.directory)
+        directory_layout.addWidget(self.search)
+
+        ### Nom du fichier
+
+        self.name_label = QLabel("Nom : ")
+        self.name_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+
+        self.name = QLineEdit("")
+        self.name.setEnabled = True
+        self.name.editingFinished.connect(self.directory_action)
+
+        name_layout.addWidget(self.name_label)
+        name_layout.addWidget(self.name)
+
+        ### Pas
+
+        self.step_size_label = QLabel("Pas : p(\u03bcm)")
+        self.step_size_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+
+        self.step_size = QLineEdit("5")
+        self.step_size.setEnabled = True
+        self.step_size.editingFinished.connect(self.step_action)
+
+        step_size_layout.addWidget(self.step_size_label)
+        step_size_layout.addWidget(self.step_size)
+
+        ### Nombre de pas
+
+        self.step_num_label = QLabel("Nombre de pas : ")
+        self.step_num_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+
+        self.step_num = QLineEdit("5")
+        self.step_num.setEnabled = True
+        self.step_num.editingFinished.connect(self.step_action)
+
+        step_num_layout.addWidget(self.step_num_label)
+        step_num_layout.addWidget(self.step_num)
+
+        ### start/stop
+
+        self.start_button = QPushButton("Start")
+        self.start_button.setStyleSheet("""
+                    QPushButton {
+                        background-color: #ee0000; 
+                        color: white;                
+                        border-radius: 5px;
+                        padding: 8px 16px;
+                    }
+                    QPushButton:hover {
+                        background-color: #ce0000;
+                    }
+                    QPushButton:pressed {
+                        background-color: #ae0000;
+                    }
+                """)
+        self.start_button.clicked.connect(self.step_action)
+
+        self.stop_button = QPushButton("Stop")
+        self.stop_button.setStyleSheet("""
+                    QPushButton {
+                        background-color: #ee0000;
+                        color: white;           
+                        border-radius: 5px;
+                        padding: 8px 16px;
+                    }
+                    QPushButton:hover {
+                        background-color: #ce0000; 
+                    }
+                    QPushButton:pressed {
+                        background-color: #ae0000; 
+                    }
+                """)
+
+        self.stop_button.clicked.connect(self.step_action)
+
+        buttons_layout.addWidget(self.start_button)
+        buttons_layout.addWidget(self.stop_button)
+
+        layout.addLayout(directory_layout)
+        layout.addLayout(name_layout)
+        layout.addLayout(step_size_layout)
+        layout.addLayout(step_num_layout)
+        layout.addLayout(buttons_layout)
+
+        self.setLayout(layout)
+
+    def directory_action(self):
+        sender = self.sender()
+        if sender == self.directory:
+            self.acqThread.emit("dir" + self.directory.text())
+            print("the file directory has been modified")
+        if sender == self.search:
+            folder_request = QFileDialog.getExistingDirectory(self,"Choisir un fichier")
+            if folder_request:
+                self.acqThread.emit("direction=" + folder_request)
+                print(f"the new destination is {folder_request}")
+                self.directory.setText(folder_request)
+        if sender == self.name:
+            self.acqThread.emit("name=" + self.name.text())
+            print(f"the name of the file will be {self.name.text()}")
+
+    def step_action(self):
+        sender = self.sender()
+        if sender == self.step_size:
+            self.acqThread.emit("StepSize=" + self.step_size.text())
+            print(f"the step size has been updated")
+        if sender == self.step_num:
+            self.acqThread.emit("StepNum=" + self.step_num.text())
+            print(f"the number of steps has been updated")
+        if sender == self.start_button:
+            self.acqThread.emit("Start=")
+            print(f"the acquisition has begun")
+        if sender == self.stop_button:
+            self.acqThread.emit("Stop=")
+            print(f"the acquisition has been stopped")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    fenetre = AcquisitionView()
+    fenetre.show()
+    sys.exit(app.exec())
